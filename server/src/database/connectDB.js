cee1cb64c3a730c87810c2ded3121c4bc210d5e4
Dhiraj Kumar <dhirajkrraj824113@gmail.com>
2026-09-11 12:57:13 +0530

    refactor: adds parallel command for client and server

--- server/src/database/connectDB.js
+++ server/src/database/connectDB.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import dns from "dns";
 import { env } from "../config/env.js"
 
 const connectDB = async () => {
     try {
+        dns.setServers(["8.8.8.8", "1.1.1.1"])
         const connection = await mongoose.connect(env.mongodbUri)
 
         console.log(`MongoDB connected: ${connection.connection.host}/${connection.connection.name}`,
